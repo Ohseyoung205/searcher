99710ff7583c7b6fb33c39491114892468a55c45
wlghsp <wlghsp@gmail.com>
2021-06-17 16:35:42 +0900

    용어검색,용어집이름 검색 CrossOrigin 어노테이션 추가

--- src/main/java/com/saltlux/khnp/searcher/search/controller/TermController.java
+++ src/main/java/com/saltlux/khnp/searcher/search/controller/TermController.java
@@ -4,6 +4,7 @@ import com.saltlux.khnp.searcher.common.CommonResponseVo;
 import com.saltlux.khnp.searcher.search.service.TermSearchService;
 import com.saltlux.khnp.searcher.search.vo.TermSearchRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +17,13 @@ public class TermController {
     TermSearchService termSearchService;
 
     @GetMapping("/search")  // 용어검색 API
+    @CrossOrigin(origins="*", allowedHeaders="*")
     public CommonResponseVo termSearch(TermSearchRequest requests) {
         return new CommonResponseVo(termSearchService.termSearch(requests));
     }
 
     @GetMapping("/source/groupBy") // 용어집이름 검색 API ---- 파라미터 없음
+    @CrossOrigin(origins="*", allowedHeaders="*")
     public CommonResponseVo sourceGroupBy(){
         return new CommonResponseVo(termSearchService.sourceGroupBy());
     }
