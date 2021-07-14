366c4e13509fc1400fea20bb6e55b46203ea8910
jhjeon <jhjeon@saltlux.com>
2021-07-14 19:27:57 +0900

    [FIX]

--- src/main/java/com/saltlux/khnp/searcher/search/helper/SpecialCharacterUtils.java
+++ src/main/java/com/saltlux/khnp/searcher/search/helper/SpecialCharacterUtils.java
@@ -2,13 +2,15 @@ package com.saltlux.khnp.searcher.search.helper;
 
 public class SpecialCharacterUtils {
 
-    private static final String regex = "()[]{}!|^+-*?:\"\\";
+    private static final String REGEX = "()[]{}!|^+-*?:\"\\";
+
+    private SpecialCharacterUtils(){}
 
     public static String replaceSpecialChar(String string) {
         StringBuilder sb = new StringBuilder();
         char[] c = string.toCharArray();
         for (int i = 0; i < c.length; i++) {
-            if (regex.indexOf(c[i]) > -1)
+            if (REGEX.indexOf(c[i]) > -1)
                 sb.append("\\");
             sb.append(c[i]);
         }
